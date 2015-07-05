82d2dbf21d862f53c8334be8eae3e7194a088fa1
htk291 <htk291@gmail.com>
2015-07-05 13:39:16 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Data::Linq - It's new $module
+
+# SYNOPSIS
+
+    use Data::Linq;
+
+# DESCRIPTION
+
+Data::Linq is ...
+
+# LICENSE
+
+Copyright (C) htk291.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+htk291 <htk291@gmail.com>
---
+++ lib/Data/Linq.pm
@@ -0,0 +1,39 @@
+package Data::Linq;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Data::Linq - It's new $module
+
+=head1 SYNOPSIS
+
+    use Data::Linq;
+
+=head1 DESCRIPTION
+
+Data::Linq is ...
+
+=head1 LICENSE
+
+Copyright (C) htk291.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+htk291 E<lt>htk291@gmail.comE<gt>
+
+=cut
+
